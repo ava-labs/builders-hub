58c2f7b489edda3293855bb9a25faf67b3dd829e
damicode18 <adebayoribukunoluwa@yahoo.com>
2025-03-12 17:01:19 +0100

    ambassador page dashboard including jobs and bounties UI :rocket:

--- app/(home)/ambassador-dao/jobs/page.tsx
+++ app/(home)/ambassador-dao/jobs/page.tsx
@@ -2,9 +2,6 @@ import { redirect } from 'next/navigation'
 
 const AmbasssadorDaoAllJobsPage = () => {
   redirect('/ambassador-dao?type=jobs')
-  return (
-    <div></div>
-  )
 }
 
 export default AmbasssadorDaoAllJobsPage
