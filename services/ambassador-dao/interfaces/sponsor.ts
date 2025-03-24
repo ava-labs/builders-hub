6ccc1e3644d1f44e1078b28262610869fcf31ac6
tofmat <tofmatt@gmail.com>
2025-03-24 01:38:42 +0100

    feat: cleanup

--- services/ambassador-dao/interfaces/sponsor.ts
+++ services/ambassador-dao/interfaces/sponsor.ts
@@ -18,3 +18,77 @@ export interface ICreateOpportunityBody {
     amount: number;
   }[];
 }
+
+export interface IOppotunityListingResponse {
+  data: IOpportunityListing[];
+  metadata: {
+    current_page: number;
+    last_page: number;
+    next_page: number;
+    per_page: number;
+    prev_page: number;
+    total: number;
+  };
+}
+
+export interface IOpportunityListing {
+  id: string;
+  title: string;
+  description: string;
+  type: "JOB" | "BOUNTY";
+  category: string;
+  status: "ALL" | "DRAFT" | "OPEN" | "IN_REVIEW" | "COMPLETED";
+  start_date: string;
+  end_date: string;
+  created_at: string;
+  total_budget: number;
+  requirements: string;
+  max_winners: number;
+  updated_at: Date;
+  skills: {
+    id: string;
+    name: string;
+  }[];
+  rewards: {
+    id: string;
+    position: number;
+    amount: number;
+    payment_status: "PENDING" | "PAYMENT_COMPLETED";
+    payment_date: Date | null;
+    transaction_id: string | null;
+    user_id: string | null;
+    submission_id: string | null;
+    created_at: string;
+    updated_at: string;
+  }[];
+  prize_distribution?: {
+    position: number;
+    endPosition?: number;
+    amount: number;
+  }[];
+  custom_questions?: string[];
+  files: string[];
+  created_by: {
+    id: string;
+    first_name: string;
+    last_name: string;
+    username: string;
+    profile_image: string;
+    company_profile: {
+      name: string;
+      logo: string;
+    };
+  };
+}
+
+export interface IOppotunityApplicationsResponse {
+  data: IOpportunityListing[];
+  metadata: {
+    current_page: number;
+    last_page: number;
+    next_page: number;
+    per_page: number;
+    prev_page: number;
+    total: number;
+  };
+}
--- services/ambassador-dao/requests/sponsor.ts
+++ services/ambassador-dao/requests/sponsor.ts
@@ -1,9 +1,14 @@
 import { errorMsg } from "@/utils/error-mapping";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { API_DEV } from "../data/constants";
-import { ICreateOpportunityBody } from "../interfaces/sponsor";
+import {
+  ICreateOpportunityBody,
+  IOpportunityListing,
+  IOppotunityApplicationsResponse,
+  IOppotunityListingResponse,
+} from "../interfaces/sponsor";
 
 export const useCreateOpportunityMutation = () => {
   return useMutation({
@@ -16,5 +21,89 @@ export const useCreateOpportunityMutation = () => {
       toast.success(data.message);
     },
     onError: (err) => errorMsg(err),
+  });
+};
+
+export const useFetchAllListings = (
+  query: string,
+  type: string,
+  page: number,
+  limit: number,
+  status: string
+) => {
+  return useQuery({
+    queryKey: ["allListings", query, type, page, limit, status],
+    queryFn: async () => {
+      const res = await axios.get(`${API_DEV}/opportunity/listings`, {
+        params: {
+          query,
+          type: type === "all" ? undefined : type,
+          page,
+          limit,
+          status,
+        },
+      });
+      return res.data as IOppotunityListingResponse;
+    },
+    staleTime: Infinity,
+  });
+};
+
+export const useFetchSingleListing = (id: string) => {
+  return useQuery({
+    queryKey: ["singleListings"],
+    queryFn: async () => {
+      const res = await axios.get(`${API_DEV}/opportunity/${id}`);
+      return res.data.data as IOpportunityListing;
+    },
+    staleTime: Infinity,
+  });
+};
+
+export const useFetchSingleListingSubmissions = (
+  id: string,
+  query: string,
+  page: number,
+  limit: number,
+  status: string
+) => {
+  return useQuery({
+    queryKey: ["singleListingsSubmissions", query, page, limit, status],
+    queryFn: async () => {
+      const res = await axios.get(`${API_DEV}/opportunity/${id}/submissions`, {
+        params: {
+          query,
+          page,
+          limit,
+          status: status === "ALL" ? undefined : status,
+        },
+      });
+      return res.data as IOppotunityApplicationsResponse;
+    },
+    staleTime: Infinity,
+  });
+};
+
+export const useFetchSingleListingApplications = (
+  id: string,
+  query: string,
+  page: number,
+  limit: number,
+  status: string
+) => {
+  return useQuery({
+    queryKey: ["singleListingsApplications", query, page, limit, status],
+    queryFn: async () => {
+      const res = await axios.get(`${API_DEV}/opportunity/${id}/applications`, {
+        params: {
+          query,
+          page,
+          limit,
+          status: status === "ALL" ? undefined : status,
+        },
+      });
+      return res.data as IOppotunityApplicationsResponse;
+    },
+    staleTime: Infinity,
   });
 };
