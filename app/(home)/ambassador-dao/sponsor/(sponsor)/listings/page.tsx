a16bbe0dc4ec5f89181aaeb41c5ff8c30f4606a5
tofmat <tofmatt@gmail.com>
2025-03-24 11:51:01 +0100

    resolve build fail

--- app/(home)/ambassador-dao/sponsor/(sponsor)/listings/page.tsx
+++ app/(home)/ambassador-dao/sponsor/(sponsor)/listings/page.tsx
@@ -37,6 +37,7 @@ import {
   useFetchUserStatsDataQuery,
 } from "@/services/ambassador-dao/requests/auth";
 import Loader from "@/components/ambassador-dao/ui/Loader";
+import { StatusBadge } from "@/components/ambassador-dao/status-badge";
 
 // Mock data
 const mockUser = {
@@ -402,34 +403,5 @@ function TabButton({ active, onClick, children }: TabButtonProps) {
     >
       {children}
     </button>
-  );
-}
-
-export function StatusBadge({ status }: { status: string }) {
-  const getStatusStyles = () => {
-    switch (status) {
-      case "Draft":
-        return "bg-[#6D6D6D]";
-      case "Live Job":
-        return "bg-[#155DFC]";
-      case "In Review":
-        return "bg-[#00D492]";
-      case "Payment Pending":
-        return "bg-[#F0B100]";
-      case "Pending Reviews":
-        return "bg-[#F0B100]";
-      case "Complete":
-        return "bg-[#E5E7EB] text-black";
-      default:
-        return "bg-gray-700";
-    }
-  };
-
-  return (
-    <span
-      className={`px-3 py-1 rounded-full text-xs font-medium ${getStatusStyles()}`}
-    >
-      {status}
-    </span>
   );
 }
