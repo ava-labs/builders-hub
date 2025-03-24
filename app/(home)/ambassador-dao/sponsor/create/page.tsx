42a83f42bc9b440a842c5af577c6c5ffcc89e6d1
tofmat <tofmatt@gmail.com>
2025-03-24 12:03:50 +0100

    resolve build fail

--- app/(home)/ambassador-dao/sponsor/create/page.tsx
+++ app/(home)/ambassador-dao/sponsor/create/page.tsx
@@ -3,17 +3,18 @@ import FullScreenLoader from "@/components/ambassador-dao/full-screen-loader";
 import AmbasssadorDaoSponsorsCreateListing from "@/components/ambassador-dao/sections/create-listing";
 import { useFetchUserDataQuery } from "@/services/ambassador-dao/requests/auth";
 import { useSearchParams, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import toast from "react-hot-toast";
 
-export default function AmbasssadorDaoSponsorsCreateListingPage() {
+function SearchParamsWrapper() {
   const searchParams = useSearchParams();
   const router = useRouter();
   const type = searchParams.get("type");
+
   if (!type) {
     toast.error("Something went wrong, please try again.");
     router.push("/ambassador-dao/sponsor/listings");
-    return;
+    return null;
   }
 
   const { data: user, isLoading } = useFetchUserDataQuery();
@@ -34,7 +35,15 @@ export default function AmbasssadorDaoSponsorsCreateListingPage() {
     <>
       {user && (
         <AmbasssadorDaoSponsorsCreateListing type={type as "JOB" | "BOUNTY"} />
-      )}{" "}
+      )}
     </>
+  );
+}
+
+export default function AmbasssadorDaoSponsorsCreateListingPage() {
+  return (
+    <Suspense fallback={<FullScreenLoader />}>
+      <SearchParamsWrapper />
+    </Suspense>
   );
 }
