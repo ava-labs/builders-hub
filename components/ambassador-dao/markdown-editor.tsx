260c1899c09bffc5cb602de2355a4af85f79a547
tofmat <tofmatt@gmail.com>
2025-03-26 14:31:00 +0100

    feat: completed markdown component for creating opportunities

---
+++ components/ambassador-dao/markdown-editor.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { ICreateOpportunityBody } from "@/services/ambassador-dao/interfaces/sponsor";
+import {
+  MDXEditor,
+  MDXEditorMethods,
+  headingsPlugin,
+  BoldItalicUnderlineToggles,
+  toolbarPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  BlockTypeSelect,
+  CreateLink,
+  InsertImage,
+  CodeToggle,
+  imagePlugin,
+  linkPlugin,
+  jsxPlugin,
+} from "@mdxeditor/editor";
+import { FC } from "react";
+import { UseFormSetValue } from "react-hook-form";
+
+interface EditorProps {
+  markdown: string;
+  editorRef?: React.MutableRefObject<MDXEditorMethods | null>;
+  setValue: UseFormSetValue<ICreateOpportunityBody>;
+}
+
+const MarkdownEditor: FC<EditorProps> = ({ markdown, editorRef, setValue }) => {
+  return (
+    <MDXEditor
+      onChange={(e) => setValue("description", e)}
+      className='custom-markdown-editor'
+      ref={editorRef}
+      markdown={markdown}
+      plugins={[
+        jsxPlugin(),
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        imagePlugin(),
+        linkPlugin(),
+        toolbarPlugin({
+          toolbarClassName: "my-classname",
+          toolbarContents: () => (
+            <>
+              <BoldItalicUnderlineToggles />
+              <BlockTypeSelect />
+              <CreateLink />
+              <InsertImage />
+              <CodeToggle />
+            </>
+          ),
+        }),
+      ]}
+    />
+  );
+};
+
+export default MarkdownEditor;
--- services/ambassador-dao/interfaces/user.ts
+++ services/ambassador-dao/interfaces/user.ts
@@ -11,7 +11,7 @@ export interface IUserDetails {
 
 export interface IVerifiedDetails {
   user: IUserDetails;
-  token: {
+  tokens: {
     access_token: string;
     refresh_token: string;
   };
