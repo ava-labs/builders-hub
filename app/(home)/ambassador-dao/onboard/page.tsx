6689ba603186876a6cfe8e2738ba1a2dd3ef25b8
tofmat <tofmatt@gmail.com>
2025-04-05 17:51:42 +0100

    feat: fixed more bugs

--- app/(home)/ambassador-dao/onboard/page.tsx
+++ app/(home)/ambassador-dao/onboard/page.tsx
@@ -158,7 +158,7 @@ const AmbasssadorDaoOnboardPage = () => {
                   onClick={() => handleContinue(type.name.toUpperCase() as any)}
                   className="px-6 h-10 text-sm font-medium"
                 >
-                  Continue as <span className="capitalize">{type.name}</span>
+                  Continue as {type.name}
                 </CustomButton>
               </div>
             </div>
