5daa322630639e0d00f444ed71b220159a67c745
damicode18 <adebayoribukunoluwa@yahoo.com>
2025-04-17 15:48:36 +0100

    revert:fix dialog issues

--- components/ambassador-dao/sections/accept-applicant.tsx
+++ components/ambassador-dao/sections/accept-applicant.tsx
@@ -1,13 +1,8 @@
 import React, { useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-} from "@/components/ambassador-dao/dialog";
 import CustomButton from "../custom-button";
 import { useReviewApplicantMutation } from "@/services/ambassador-dao/requests/sponsor";
 import CustomInput from "../input";
-import { Dialog, DialogContent, DialogTitle } from "../ui/dialog";
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 
 interface IDeleteProps {
   isOpen: boolean;
