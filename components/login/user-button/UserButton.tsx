c046385ec941194fe1858b080dc8ffba80638282
anotherdev <joseluismanco37@gmail.com>
2025-04-17 16:46:50 -0500

    feat: add profile button

--- components/login/user-button/UserButton.tsx
+++ components/login/user-button/UserButton.tsx
@@ -6,26 +6,24 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { signOut, useSession } from "next-auth/react";
-import Image from "next/image";
-import Link from "next/link";
-import SignOutComponent from "../sign-out/SignOut";
-import { useState } from "react";
-import { UserRound, CircleUserRound } from "lucide-react";
+} from '@/components/ui/dropdown-menu';
+import { signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import SignOutComponent from '../sign-out/SignOut';
+import { useState } from 'react';
+import { CircleUserRound, UserRound } from 'lucide-react';
 
 export function UserButton() {
   const { data: session, status } = useSession();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const isAuthenticated = status === "authenticated";
-
+  const isAuthenticated = status === 'authenticated';
   const handleSignOut = (): void => {
     signOut();
   };
-
   return (
     <>
-      {isAuthenticated ? (
+      {isAuthenticated && session?.user?.image ? (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button
@@ -33,23 +31,27 @@ export function UserButton() {
               size='icon'
               className='rounded-full h-10 w-10 ml-4 cursor-pointer p-1'
             >
-              {session?.user?.image ? (
-                <Image
-                  src={session.user.image}
-                  alt="User Avatar"
-                  width={20}
-                  height={20}
-                  className="rounded-full"
-                />
-              ) : (
-                <UserRound className="h-5 w-5" />
-              )}
+              <Image
+                src={session.user.image}
+                alt='User Avatar'
+                width={32}
+                height={32}
+                className='rounded-full'
+              />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent className="bg-white text-black dark:bg-zinc-900 dark:text-white border border-zinc-200 dark:border-zinc-600 shadow-lg p-1 rounded-md w-48">
-            <DropdownMenuItem onClick={() => setIsDialogOpen(true)} className="cursor-pointer">
+          <DropdownMenuContent
+            className='bg-white text-black dark:bg-zinc-900 dark:text-white
+    border border-zinc-200 dark:border-zinc-600
+    shadow-lg p-1 rounded-md w-48'
+          >
+            <DropdownMenuItem>
+              <Link href="/profile">Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setIsDialogOpen(true)}>
               Sign Out
             </DropdownMenuItem>
+
           </DropdownMenuContent>
         </DropdownMenu>
       ) : (
