c96051850e28fb9113476e605ee9a0783da6df0a
anotherdev <joseluismanco37@gmail.com>
2025-04-17 19:25:09 -0500

    fix: not focus on tab

--- components/hackathons/project-submission/components/FormLabelWithCheck.tsx
+++ components/hackathons/project-submission/components/FormLabelWithCheck.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { FC } from "react";
+import { FC } from 'react';
 
-import { FormLabel } from "@/components/ui/form";
-import { Checkbox } from "@/components/ui/checkbox";
-import { cn } from "@/lib/utils"
+import { FormLabel } from '@/components/ui/form';
+import { Checkbox } from '@/components/ui/checkbox';
+import { cn } from '@/lib/utils';
 
 interface FormLabelWithCheckProps {
   label: string;
@@ -16,20 +16,18 @@ export const FormLabelWithCheck: FC<FormLabelWithCheckProps> = ({
   checked,
 }) => {
   return (
-    <div className="w-full flex  px-4 py-2 rounded-md text-sm  dark:bg-zinc-800 ">
+    <div className='w-full flex  px-4 py-2 rounded-md text-sm  dark:bg-zinc-800 '>
       <Checkbox
+        disabled
         checked={checked}
+        tabIndex={-1}
         className={cn(
-            "pointer-events-none opacity-100 border dark:border-white mr-2 rounded-md", 
-            "dark:data-[state=checked]:bg-white data-[state=checked]:bg-white",
-                "[&_[data-slot='checkbox-indicator']_svg]:stroke-black"
-          )}
- 
-        
+          'pointer-events-none opacity-100 border dark:border-white mr-2 rounded-md',
+          'dark:data-[state=checked]:bg-white data-[state=checked]:bg-white',
+          "[&_[data-slot='checkbox-indicator']_svg]:stroke-black"
+        )}
       />
-      <FormLabel className="m-0 p-0 cursor-default ">
-        {label}
-      </FormLabel>
+      <FormLabel className='m-0 p-0 cursor-default '>{label}</FormLabel>
     </div>
   );
 };
