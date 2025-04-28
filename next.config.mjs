6bcbd44bd32edcf00f7e3716cca43d8a16339dff
ADEBAYO DAMILARE <49120752+DamiCode18@users.noreply.github.com>
2025-04-28 03:27:28 +0100

    Bug fixes4 (#95)
    * export as cvs and xlsx
    
    * fixes

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,10 @@ const config = {
       },
       {
         protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
       {
