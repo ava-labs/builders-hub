2a3dc2446fb1ad08f007170beacedcb982f3e12d
sebaxor <sebasor@gmail.com>
2025-04-28 19:58:19 -0500

    choring load testing script

--- load-tests/autocannon/run.sh
+++ load-tests/autocannon/run.sh
@@ -2,19 +2,19 @@
 
 echo "Running Autocannon Ramp-Up Test..."
 
-# Etapa 1
-npx autocannon -c 50 -d 60 -p 10 --json https://tu-app.vercel.app/hackathons > autocannon-50c.json
+# Stage 1
+npx autocannon -c 50 -d 60 -p 10 --json https://avalanche-docs-eight.vercel.app/hackathons > autocannon-50c.json
 
-# Etapa 2
-npx autocannon -c 100 -d 120 -p 10 --json https://tu-app.vercel.app/hackathons > autocannon-100c.json
+# Stage 2
+npx autocannon -c 100 -d 120 -p 10 --json https://avalanche-docs-eight.vercel.app/hackathons > autocannon-100c.json
 
-# Etapa 3
-npx autocannon -c 150 -d 120 -p 10 --json https://tu-app.vercel.app/hackathons > autocannon-150c.json
+# Stage 3
+npx autocannon -c 150 -d 120 -p 10 --json https://avalanche-docs-eight.vercel.app/hackathons > autocannon-150c.json
 
-# Etapa 4
-npx autocannon -c 200 -d 180 -p 10 --json https://tu-app.vercel.app/hackathons > autocannon-200c.json
+# Stage 4
+npx autocannon -c 200 -d 180 -p 10 --json https://avalanche-docs-eight.vercel.app/hackathons > autocannon-200c.json
 
-# Etapa 5
-npx autocannon -c 300 -d 300 -p 10 --json https://tu-app.vercel.app/hackathons > autocannon-300c.json
+# Stage 5
+npx autocannon -c 300 -d 300 -p 10 --json https://avalanche-docs-eight.vercel.app/hackathons > autocannon-300c.json
 
 echo "Ramp-up test completed. JSON results saved."
