938509aec483e1b796453ece5271dd5643c8a51f
Containerman17 <8990432+containerman17@users.noreply.github.com>
2025-05-30 05:33:58 +0000

    lint

--- toolbox/src/toolbox/Nodes/BlockScout.tsx
+++ toolbox/src/toolbox/Nodes/BlockScout.tsx
@@ -1,25 +1,15 @@
 "use client";
 
-import { useWalletStore } from "../../stores/walletStore";
 import { useState, useEffect, useMemo } from "react";
-import { networkIDs } from "@avalabs/avalanchejs";
-import versions from "../../versions.json";
 import { Container } from "../../components/Container";
 import { Input } from "../../components/Input";
 import { getBlockchainInfo } from "../../coreViem/utils/glacier";
 import InputChainId from "../../components/InputChainId";
-import { Checkbox } from "../../components/Checkbox";
 
 import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 import { Steps, Step } from "fumadocs-ui/components/steps";
 import { DynamicCodeBlock } from 'fumadocs-ui/components/dynamic-codeblock';
-import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
-import { AddChainModal } from "../../components/ConnectWallet/AddChainModal";
-import { useL1ListStore } from "../../stores/l1ListStore";
-import { Button } from "../../components/Button";
-import { RadioGroup } from "../../components/RadioGroup";
-import { Success } from "../../components/Success";
-import { dockerInstallInstructions, nodeConfigBase64, type OS } from "./AvalanchegoDocker";
+import { dockerInstallInstructions, type OS } from "./AvalanchegoDocker";
 
 const genCaddyfile = (domain: string) => `
 ${domain} {
