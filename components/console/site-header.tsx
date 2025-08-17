dfe4776ce0e3cb66c28deb3cd73b044d37c06165
Martin Eckardt <m.eckardt@outlook.com>
2025-08-18 01:01:57 +0200

    rounded top site header

--- components/console/site-header.tsx
+++ components/console/site-header.tsx
@@ -24,7 +24,7 @@ export function SiteHeader() {
   const breadcrumbs = useBreadcrumbs(routeMetadata);
 
   return (
-    <header className="sticky top-0 z-50 flex h-(--header-height) shrink-0 items-center gap-2 border-b backdrop-blur  transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="sticky top-0 z-50 flex h-(--header-height) shrink-0 items-center gap-2 border-b backdrop-blur  transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height) rounded-t-2xl">
 
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <WalletBootstrap />
