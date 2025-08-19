85915f7d493e722bfb72de2389a6ae3a3896cb0d
navillanueva <nicolasarnedo99@gmail.com>
2025-08-19 15:21:03 +0200

    redirects for new course

--- next.config.mjs
+++ next.config.mjs
@@ -237,6 +237,12 @@ const config = {
         source: "/docs/tooling/avalanche-cli",
         destination: "/docs/tooling/cli-commands",
         permanent: true,
+      }, {
+        source: "/academy/l1-validator-management",
+        destination: "/academy/permissioned-l1s",
+        permanent: true,
+      }, {
+        source: "/docs/tooling/avalanche-cli#network-status",
       }
     ];
   },
