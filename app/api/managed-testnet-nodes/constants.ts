da59d8e6e406287787ccdb3095cc7bb454516532
Owen <owenwahlgren@gmail.com>
2025-08-26 10:03:30 -0400

    nit: service url name

--- app/api/managed-testnet-nodes/constants.ts
+++ app/api/managed-testnet-nodes/constants.ts
@@ -1,13 +1,13 @@
-export const BUILDER_HUB_BASE_URL = 'https://multinode-experimental.solokhin.com';
+export const MANAGED_TESTNET_NODES_SERVICE_URL = 'https://multinode-experimental.solokhin.com';
 
 // Managed Testnet Nodes service endpoints
 export const ManagedTestnetNodesServiceURLs = {
   addNode: (subnetId: string, password: string) => 
-    `${BUILDER_HUB_BASE_URL}/node_admin/subnets/add/${subnetId}?password=${password}`,
+    `${MANAGED_TESTNET_NODES_SERVICE_URL}/node_admin/subnets/add/${subnetId}?password=${password}`,
   
   deleteNode: (subnetId: string, nodeIndex: number, password: string) =>
-    `${BUILDER_HUB_BASE_URL}/node_admin/subnets/delete/${subnetId}/${nodeIndex}?password=${password}`,
+    `${MANAGED_TESTNET_NODES_SERVICE_URL}/node_admin/subnets/delete/${subnetId}/${nodeIndex}?password=${password}`,
   
   rpcEndpoint: (blockchainId: string) =>
-    `${BUILDER_HUB_BASE_URL}/ext/bc/${blockchainId}/rpc`
+    `${MANAGED_TESTNET_NODES_SERVICE_URL}/ext/bc/${blockchainId}/rpc`
 };
