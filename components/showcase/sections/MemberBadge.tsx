9ee31b5fe1d467acd2f02db42405321b0672ff36
jhoan <jhoan_sebastian20@hotmail.com>
2025-08-25 19:03:32 -0500

    refactor: update MemberBadge layout for improved user experience
    - Changed the layout of the MemberBadge component to a flex column structure.
    - Added a header for badges to enhance clarity and organization.
    - Maintained existing badge display functionality while improving overall presentation.

--- components/showcase/sections/MemberBadge.tsx
+++ components/showcase/sections/MemberBadge.tsx
@@ -10,26 +10,32 @@ export default function MemberBadge({badges, member}: {badges: UserBadge[], memb
   return (
     <>
       {badgesFiltered && badgesFiltered.length > 0 ? (
-        <div className="flex flex-wrap gap-1.5 p-2">
-          {badgesFiltered.map((badge) => (
-            <div 
-              key={badge.badge_id} 
-              className="flex items-center justify-center w-16 h-16 rounded-full bg-gray-100 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 hover:scale-110 transition-transform duration-200 overflow-hidden"
-              title={badge.name}
-            >
-              <Image
-                src={badge.image_path}
-                alt={badge.name + " icon"}
-                width={64}
-                height={64}
-                className="object-cover w-full h-full"
-                draggable={false}
-                quality={90}
-                priority={false}
-                unoptimized={false}
-              />
-            </div>
-          ))}
+        <div className="flex flex-col gap-2 p-2">
+          <h2 className="text-sm text-gray-500 dark:text-gray-400">Badges</h2>
+          
+          <div className="flex flex-wrap gap-1.5">
+            {badgesFiltered.map((badge) => (
+            
+              <div 
+                key={badge.badge_id} 
+                className="flex items-center justify-center w-16 h-16 rounded-full bg-gray-100 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 hover:scale-110 transition-transform duration-200 overflow-hidden"
+                title={badge.name}
+              >
+        
+                <Image
+                  src={badge.image_path}
+                  alt={badge.name + " icon"}
+                  width={64}
+                  height={64}
+                  className="object-cover w-full h-full"
+                  draggable={false}
+                  quality={90}
+                  priority={false}
+                  unoptimized={false}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       ) : (
         <div className="p-2">
