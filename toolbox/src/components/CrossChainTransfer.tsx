a68c50060d679e0c5bda3bd4117ecd3c05815892
0xstt <sarp.tomruk@avalabs.org>
2025-08-28 00:13:54 +0300

    fetch isTestnet safely

--- toolbox/src/components/CrossChainTransfer.tsx
+++ toolbox/src/components/CrossChainTransfer.tsx
@@ -52,11 +52,11 @@ export default function CrossChainTransfer({
 
     const {
         coreWalletClient,
-        isTestnet,
         updateCChainBalance,
         updatePChainBalance
     } = useWalletStore()
 
+    const isTestnet = useWalletStore((s) => s.isTestnet);
     const cChainBalance = useWalletStore((s) => s.balances.cChain);
     const pChainBalance = useWalletStore((s) => s.balances.pChain);
     const pChainAddress = useWalletStore((s) => s.pChainAddress);
