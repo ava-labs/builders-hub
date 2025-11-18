82405b7817fd2e2a236d32a61bf29a47fc2ac8aa
jhoan <sebastianmosquera1075@gmail.com>
2025-11-18 09:57:47 -0500

    Remove commented-out badge unlocking logic in RewardBoard component for cleaner code.

--- components/profile/reward-board/component/reward-board.tsx
+++ components/profile/reward-board/component/reward-board.tsx
@@ -19,18 +19,18 @@ export default async function RewardBoard() {
   
   const academyBadges = badges.filter((badge) => badge.category == "academy")?.sort((a, b) => a.id.localeCompare(b.id));
   const hackathonBadges: Badge[] = badges.filter((badge) => badge.category == "hackathon")?.sort((a, b) => a.id.localeCompare(b.id));
-  const totalPoints = userBadges.reduce((acc, userBadge) => acc + userBadge.points, 0);
-  const hackathonBadgesUnlocked = hackathonBadges.map((badge) => {
-    const userBadge = userBadges.find((userBadge) => userBadge.badge_id == badge.id);
+  // const totalPoints = userBadges.reduce((acc, userBadge) => acc + userBadge.points, 0);
+  // const hackathonBadgesUnlocked = hackathonBadges.map((badge) => {
+  //   const userBadge = userBadges.find((userBadge) => userBadge.badge_id == badge.id);
     
-    const allRequirementsCompleted = userBadge?.requirements && userBadge.requirements.length > 0 &&
-      userBadge.requirements.every((requirement) => requirement.unlocked === true);
-    return {
-      ...badge,
-      is_unlocked: !!allRequirementsCompleted,
-      requirements: userBadge?.requirements || badge.requirements,
-    };
-  });
+  //   const allRequirementsCompleted = userBadge?.requirements && userBadge.requirements.length > 0 &&
+  //     userBadge.requirements.every((requirement) => requirement.unlocked === true);
+  //   return {
+  //     ...badge,
+  //     is_unlocked: !!allRequirementsCompleted,
+  //     requirements: userBadge?.requirements || badge.requirements,
+  //   };
+  // });
 
   const academyBadgesUnlocked = academyBadges.map((badge) => {
     const userBadge = userBadges.find((userBadge) => userBadge.badge_id == badge.id);
