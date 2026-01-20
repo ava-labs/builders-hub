52709cfc3242b3dc0a186d851ca309357ab861b8
Ashutosh Tripathi <ashucoder9@gmail.com>
2026-01-20 15:28:49 +0530

    default dark mode

--- app/(home)/build-games/layout.tsx
+++ app/(home)/build-games/layout.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import type { ReactNode } from "react";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
 
 export default function BuildGamesLayout({
   children,
 }: {
   children: ReactNode;
 }): React.ReactElement {
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    setTheme("dark");
+  }, [setTheme]);
+
   return <>{children}</>;
 }
