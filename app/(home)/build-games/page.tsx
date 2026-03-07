895e57d80894e2904e8566898c3d1151518fdc1b
Andrea Vargas <andrea.vargas@avalabs.org>
2026-03-06 23:03:17 -0600

    feat(build games): remove application tracker to prevent confusion from stage tracking

--- app/(home)/build-games/page.tsx
+++ app/(home)/build-games/page.tsx
@@ -2,7 +2,6 @@ import clsx from "clsx";
 import Link from "next/link";
 import "./styles.css";
 import ReferralLink from "@/components/build-games/ReferralLink";
-import ApplicationStatusTracker from "@/components/build-games/ApplicationStatusTracker";
 import ProgramTimelineWrapper from "@/components/build-games/ProgramTimelineWrapper";
 import BuildGamesResourcesWrapper from "@/components/build-games/BuildGamesResourcesWrapper";
 import HowItWorksWrapper from "@/components/build-games/HowItWorksWrapper";
@@ -1025,7 +1024,6 @@ function MainContent() {
     <div className="absolute content-stretch flex flex-col items-start left-[-0.05px] right-[0.05px] top-0" data-name="Main">
       <HeroSection />
       <HeroTilesSection />
-      <ApplicationStatusTracker />
       <div className="relative shrink-0 w-full" data-name="CTA">
         <div className="flex flex-col justify-center size-full">
           <div className="content-stretch flex flex-col gap-[16px] items-start justify-center px-[186px] py-0 relative w-full">
