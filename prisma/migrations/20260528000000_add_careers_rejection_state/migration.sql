76a8dc40c472c4deb2d60f5526ab4562f6a08e59
navillanueva <nicolasarnedo99@gmail.com>
2026-05-27 22:20:50 -0400

    chore(careers): remove nit

--- prisma/migrations/20260528000000_add_careers_rejection_state/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN "careers_rejected_at" TIMESTAMPTZ(3);
-
--- AlterTable
-ALTER TABLE "JobListing" ADD COLUMN "rejected_at" TIMESTAMPTZ(3);
-
--- CreateIndex
-CREATE INDEX "JobListing_rejected_at_idx" ON "JobListing"("rejected_at");
