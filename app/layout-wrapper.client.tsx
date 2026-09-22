49b81a10710347dd68d65d377a114ebfa1e5839e
alejandro <alejosotodiaz@hotmail.com>
2026-09-22 11:06:04 -0500

    chore(blog): mark helicon as live on mainnet

--- app/layout-wrapper.client.tsx
+++ app/layout-wrapper.client.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from 'react';
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import { useSession } from 'next-auth/react';
 import { ActiveNavHighlighter } from '@/components/navigation/active-nav-highlighter';
-import { CustomCountdownBanner } from '@/components/ui/custom-countdown-banner';
+// import { CustomCountdownBanner } from '@/components/ui/custom-countdown-banner';
 import { hasTeam1AcademyAccess } from '@/lib/auth/roles';
 
 interface LayoutWrapperProps {
@@ -51,7 +51,7 @@ export function LayoutWrapper({ children, baseOptions }: LayoutWrapperProps) {
   return (
     <>
       <ActiveNavHighlighter />
-      <CustomCountdownBanner />
+      {/* <CustomCountdownBanner /> */}
       <HomeLayout {...updatedOptions}>{children}</HomeLayout>
     </>
   );
